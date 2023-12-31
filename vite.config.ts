3d45985da6c28b5f79e29cb6bfb06db660eadc99
Jonathan <jonathanbuko@icloud.com>
2023-12-31 17:23:08 +0100

    update base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: "/to-do-list/",
+  base: "/To-DO-list/",
 });
